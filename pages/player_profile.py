9a25e1a5ac9ac76603f97a35f28780a76cac41a9
dhill993 <Dhillon_16@live.co.uk>
2025-07-09 14:21:36 +0100

    Update player_profile.py

--- pages/player_profile.py
+++ pages/player_profile.py
@@ -1,5 +1,8 @@
 import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "data")))
+import sys
+import os
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
