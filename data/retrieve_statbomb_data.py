2a50710fb73c31ed5b9883c75c446119e1b903e4
dhill993 <Dhillon_16@live.co.uk>
2025-09-01 09:44:11 +0100

    Update retrieve_statbomb_data.py

--- data/retrieve_statbomb_data.py
+++ data/retrieve_statbomb_data.py
@@ -149,7 +149,7 @@ def get_statsbomb_player_season_stats():
             df['Position'] = df['Position'].map(position_mapping)
             df = df.dropna(subset=['Position'])
             df['Position'] = df['Position'].astype(str).str.strip()
-            df = df[df['Minutes'] >= 600]
+            df = df[df['Minutes'] >= 500]
             df['Minutes'] = df['Minutes'].astype(int)
 
             dataframes.append(df)
