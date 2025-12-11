3a76a284a4c7fd65c5509d90f1fd7ac2e9c25328
dhill993 <Dhillon_16@live.co.uk>
2025-12-11 15:16:22 +0000

    Update weighted_rank.py

--- visualizations/weighted_rank.py
+++ visualizations/weighted_rank.py
@@ -5,12 +5,12 @@ def get_weighted_rank(data, player_name, league_name, season, position, api='sta
     data = get_overall_rank(data, league_name, season, position, api)
     data = data[data['Player Name'] == player_name]
     points_of_this_league = get_weighted_score(league_name)
-    points_of_league_two = get_weighted_score("League two")
+    points_of_league_Two = get_weighted_score("League Two")
 
-    factor = points_of_this_league/points_of_league_two
+    factor = points_of_this_league/points_of_league_Two
 
     data = data[['Player Name', 'Age', 'Minutes', 'Overall Score']]
-    data['Score weighted aganist League two'] =  data['Overall Score']*factor
+    data['Score weighted aganist League Two'] =  data['Overall Score']*factor
     data.reset_index(drop=True, inplace=True)
 
-    return data[['Player Name', 'Age', 'Minutes', 'Overall Score', 'Score weighted aganist League two']]
+    return data[['Player Name', 'Age', 'Minutes', 'Overall Score', 'Score weighted aganist League Two']]
